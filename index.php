526ebd99078d9d74cacd989dac8baa072846830f
Tomasz Szlapa <tomasz@mojorank.com>
2020-12-22 19:45:56 +0100

    add index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+    <title>
+        PHP Test bicz
+    </title>
+    </head>
+    <body>
+        
+        <?php echo '<p>Hello world!</p>'; ?>
+        <?php phpinfo(); ?>
+    </body>
+</html>
